f769fbb60b012baa0e82c11a4484ad86b21a8d73
Elmer Bulthuis <elmerbulthuis@gmail.com>
2019-09-18 17:16:14 +0200

    initial reduce trait

---
+++ src/reduce.rs
@@ -0,0 +1,56 @@
+pub trait Reduce<Action> {
+    fn reduce(&self, action: &Action) -> Self;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    struct LampState {
+        power: bool,
+    }
+
+    enum LampOnOffAction {
+        TurnOn,
+        TurnOff,
+    }
+
+    impl Reduce<LampOnOffAction> for LampState {
+        fn reduce(&self, action: &LampOnOffAction) -> Self {
+            match action {
+                LampOnOffAction::TurnOn => LampState { power: true },
+                LampOnOffAction::TurnOff => LampState { power: false },
+            }
+        }
+    }
+
+    enum LampSwitchAction {
+        Switch,
+    }
+
+    impl Reduce<LampSwitchAction> for LampState {
+        fn reduce(&self, action: &LampSwitchAction) -> Self {
+            match action {
+                LampSwitchAction::Switch => LampState { power: !self.power },
+            }
+        }
+    }
+
+    #[test]
+    fn reduce_test() {
+        let state = LampState { power: false };
+        assert_eq!(state.power, false);
+
+        let state = state.reduce(&LampOnOffAction::TurnOn);
+        assert_eq!(state.power, true);
+
+        let state = state.reduce(&LampOnOffAction::TurnOff);
+        assert_eq!(state.power, false);
+
+        let state = state.reduce(&LampSwitchAction::Switch);
+        assert_eq!(state.power, true);
+
+        let state = state.reduce(&LampSwitchAction::Switch);
+        assert_eq!(state.power, false);
+    }
+}
