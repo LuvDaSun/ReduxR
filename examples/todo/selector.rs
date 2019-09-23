21ae8041e2741553eaaf890b72206728816b2fd6
Elmer Bulthuis <elmerbulthuis@gmail.com>
2019-09-23 16:19:12 +0200

    fixing selectors

--- examples/todo/selector.rs
+++ examples/todo/selector.rs
@@ -1,36 +1,35 @@
 use super::*;
 
 pub struct TodoSelectItem {
-    id:String,
-    name:String,
-    done:bool,
+    pub id: String,
+    pub name: String,
+    pub done: bool,
 }
 
 pub struct SelectTodoItemArg {
     id: String,
 }
 
-impl TodoExampleState{
-    pub fn select_todo_count(&self) -> usize{
+impl TodoExampleState {
+    pub fn select_todo_count(&self) -> usize {
         self.todos.len()
     }
 
-    pub fn select_todo_list(&self) -> impl Iterator{
-        self.todos.iter().map(|(id,item)|TodoSelectItem{
-            id: id.clone(),
-            name: item.name.clone(),
-            done: item.done,
-        }).collect()
-    }
+    // pub fn select_todo_list(&self) -> impl Iterator{
+    //     self.todos.iter().map(|(id,item)|TodoSelectItem{
+    //         id: id.clone(),
+    //         name: item.name.clone(),
+    //         done: item.done,
+    //     }).collect()
+    // }
 
-    pub fn select_todo_item(&self, SelectTodoItemArg{id}:SelectTodoItemArg) -> TodoSelectItem{
+    pub fn select_todo_item(&self, SelectTodoItemArg { id }: SelectTodoItemArg) -> TodoSelectItem {
         let item = self.todos.get(&id).unwrap();
 
-        TodoSelectItem{
+        TodoSelectItem {
             id,
             name: item.name.clone(),
             done: item.done,
         }
     }
-
 }
