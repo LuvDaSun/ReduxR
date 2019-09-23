a128bdc1626641d718ae760e97fa6021fedb0903
Elmer Bulthuis <elmerbulthuis@gmail.com>
2019-09-23 13:28:29 +0200

    adds selectors

--- examples/todo/selector.rs
+++ examples/todo/selector.rs
@@ -1,7 +1,36 @@
 use super::*;
 
+pub struct TodoSelectItem {
+    id:String,
+    name:String,
+    done:bool,
+}
+
+pub struct SelectTodoItemArg {
+    id: String,
+}
+
 impl TodoExampleState{
     pub fn select_todo_count(&self) -> usize{
         self.todos.len()
     }
+
+    pub fn select_todo_list(&self) -> impl Iterator{
+        self.todos.iter().map(|(id,item)|TodoSelectItem{
+            id: id.clone(),
+            name: item.name.clone(),
+            done: item.done,
+        }).collect()
+    }
+
+    pub fn select_todo_item(&self, SelectTodoItemArg{id}:SelectTodoItemArg) -> TodoSelectItem{
+        let item = self.todos.get(&id).unwrap();
+
+        TodoSelectItem{
+            id,
+            name: item.name.clone(),
+            done: item.done,
+        }
+    }
+
 }
