a8c016e206679d3e9d2585e603922cef7e0103cd
Elmer Bulthuis <elmerbulthuis@gmail.com>
2019-09-27 20:46:10 +0200

    a test!

--- examples/async/stores.rs
+++ examples/async/stores.rs
@@ -1,10 +1,26 @@
+extern crate futures;
 extern crate reduxr;
 
 use super::*;
 use reduxr::*;
+use std::future::*;
+use std::pin::Pin;
+use std::task::Context;
+use std::task::Poll;
 
-pub fn create_store() -> Store<AsyncExampleState, ()> {
-    Store::default()
+#[derive(Default)]
+pub struct ReadyFuture;
+
+impl Future for ReadyFuture {
+    type Output = ();
+
+    fn poll(self: Pin<&mut Self>, _: &mut Context<'_>) -> Poll<Self::Output> {
+        Poll::Ready(())
+    }
+}
+
+pub fn create_store() -> Store<AsyncExampleState, (), ReadyFuture> {
+    Store::default().add_middleware(|context| context.dispatch_next(context.action))
 }
 
 #[cfg(test)]
@@ -13,16 +29,18 @@ mod tests {
 
     #[test]
     fn test_store() {
-        let store = create_store();
+        futures::executor::block_on(async {
+            let store = create_store();
 
-        let state = store.get_state();
-        assert_eq!(state.select_count(), 0);
+            let state = store.get_state();
+            assert_eq!(state.select_count(), 0);
 
-        store.dispatch(&());
-        store.dispatch(&());
-        store.dispatch(&());
+            store.dispatch(&()).await;
+            store.dispatch(&()).await;
+            store.dispatch(&()).await;
 
-        let state = store.get_state();
-        assert_eq!(state.select_count(), 3);
+            let state = store.get_state();
+            assert_eq!(state.select_count(), 3);
+        })
     }
 }
